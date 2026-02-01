accef48c956ec1a031f34b3f05ce7477555ad680
GraffAir <humainabsurde@proton.me>
2026-02-01 21:29:05 +0100

    feat: création de App.vue

---
+++ app/app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Asimovian&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Boldonse&display=swap');
+
+html {
+  --primary-color: #EFD690;
+  --accent-color: #7327d1;
+  --background-color: #0C0537;
+}
+
+body {
+  background: fixed url("/images/stars.png"), var(--background-color);
+}
+
+p, h1, h2, h3, h4, h5, h6, a {
+  color:white;
+}
+</style>
