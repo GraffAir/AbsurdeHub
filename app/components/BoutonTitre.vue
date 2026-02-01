369d7f31a4550e65282f8dfaebbacf1f0119e290
GraffAir <humainabsurde@proton.me>
2026-02-01 21:28:33 +0100

    feat: BoutonTitre

---
+++ app/components/BoutonTitre.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+defineProps<{texte: string, to: string}>();
+</script>
+
+<template>
+  <p>
+  <NuxtLink :to="to">
+    <img src='/images/arrow.svg' alt='>' width="30" height="20">
+    {{texte}}
+    <img src='/images/arrow.svg' alt='>' width="30" height="20" style="transform: scaleX(-1)"></NuxtLink>
+  </p>
+</template>
+
+<style scoped>
+p {
+  align-content: center;
+}
+
+a {
+  color:white;
+  text-decoration: none;
+  font-size:26pt;
+  font-family: 'Asimovian', "Roboto Light", sans-serif;
+}
+
+a img {
+  display:inline;
+  visibility: hidden;
+  margin:8px;
+}
+
+a:hover {
+  color: var(--accent-color)
+}
+
+a:hover img {
+  visibility: visible;
+}
+</style>
