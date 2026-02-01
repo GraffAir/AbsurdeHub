e37855c439cd768f5d8676df5d761fc73037ee34
GraffAir <humainabsurde@proton.me>
2026-02-01 21:28:51 +0100

    feat: ajout de l'index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    <NuxtLink>Commencer l'expédition</NuxtLink>
-    <NuxtLink>Commencer l'expédition</NuxtLink>
+    <div class="titre">
+      <p class="titre-nom">Rafaël Graff</p>
+      <h1>Créer du sens<br/> dans l&apos;<span class="titre-accent">absurde</span></h1>
+      <div class="titre-boutons">
+        <BoutonTitre to="/profil" texte="Commencer l'expédition"/>
+        <BoutonTitre to="/profil" texte="Mon profil" />
+        <BoutonTitre to="/profil" texte="Carte des missions" />
+      </div>
+    </div>
+
 
   </div>
 </template>
 <script setup lang="ts">
 </script>
-<style>
+<style scoped>
+h1 {
+  font-size:52pt;
+  font-family: 'Boldonse', sans-serif;
+}
+
+.titre {
+  padding-top:10%;
+  padding-left:10%;
+  color:white;
+  z-index:2;
+}
+
+.titre-accent {
+  font-weight: 900;
+  color: var(--accent-color);
+}
+
+.titre-boutons {
+  margin-top:5vh;
+}
+
+
 </style>
--- app/pages/profil.vue
+++ app/pages/profil.vue
@@ -7,12 +7,5 @@
 </template>
 
 <style>
-body {
-  --background-color: #41245c;
-  background:var(--background-color);
-}
 
-p, h1, h2, h3, h4, h5, h6 {
-  color:white;
-}
 </style>
