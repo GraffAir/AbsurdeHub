39d60501e936f8dca9aa36c398f98af2e234e8b8
Rafaël Graff <rafael.graff@etu.umontpellier.fr>
2026-02-03 13:46:13 +0100

    chore: extension strapi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/strapi'],
+  strapi: {
+    url: 'http://localhost:1337',
+      prefix: '/api',
+      version: 'v5',
+  }
 })
