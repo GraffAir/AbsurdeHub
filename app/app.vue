d76ad52ee9236b00dd73539e63f7fce6360958cc
GraffAir <humainabsurde@proton.me>
2026-02-05 01:38:38 +0100

    feat: style général des paragraphes

--- app/app.vue
+++ app/app.vue
@@ -10,6 +10,8 @@
 @import url('https://fonts.googleapis.com/css2?family=Asimovian&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Boldonse&display=swap');
 
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+
 html {
   --primary-color: #EFD690;
   --accent-color: #7327d1;
@@ -22,5 +24,6 @@ body {
 
 p, h1, h2, h3, h4, h5, h6, a {
   color:white;
+  font-family: "Ubuntu", serif;
 }
 </style>
