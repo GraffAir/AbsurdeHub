70f22a3f271a9944eb23e695b03677c3840c6dd1
GraffAir <humainabsurde@proton.me>
2026-02-05 04:07:24 +0100

    feat: affichage du projet amélioré

--- app/pages/missions/[url].vue
+++ app/pages/missions/[url].vue
@@ -7,6 +7,7 @@ const projets: Projet[] = response.data;
 const url = route.params.url;
 
 let projet: Projet | undefined = undefined;
+
 projets.forEach((e) => {
   console.log("Test: '"+e.url+"' est-il '"+url+"' ?");
   if (e.url == url){
@@ -15,6 +16,7 @@ projets.forEach((e) => {
   }
   console.log("Non !");
 });
+
 if (projet == undefined){
   console.log("Non trouvé ! Abandon !");
   showError({
@@ -26,15 +28,54 @@ if (projet == undefined){
 </script>
 
 <template>
-<main>
-  <MDC :value="projet.description" />
-</main>
+  <header>
+    <div class="retour">
+      <NuxtLink to="/profil">Retour au profil</NuxtLink>
+    </div>
+    <div class="titre"><h1>
+      {{projet.titre}}
+    </h1></div>
+  </header>
+  <main>
+    <MDC :value="projet.description" />
+    <div v-for="savoir in projet.savoirs">
+
+    </div>
+  </main>
 </template>
 
 <style scoped>
-main {
-  background: rgba(0, 0, 0, 0.38);
+main, header {
   margin:20px 10%;
+}
+
+main, header > div {
+  background: rgba(0, 0, 0, 0.38);
+}
+
+main {
   padding: 40px;
+}
+
+header {
+  display: flex;
+  justify-content: flex-start;
+  gap:20px;
+}
+
+header > div {
+  width:10%;
+  padding: 10px 20px;
+}
+
+.retour {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.titre {
+  flex-grow: 1;
 }
 </style>
