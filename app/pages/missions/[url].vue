fadfb7a1973b41f8c90a68c8d1363a0fd4399728
GraffAir <humainabsurde@proton.me>
2026-02-05 01:20:52 +0100

    feat: import projet depuis url

---
+++ app/pages/missions/[url].vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const route = useRoute();
+const { find } = useStrapi();
+
+const response = await find<Projet>('projets');
+const projets: Projet[] = response.data;
+const url = route.params.url;
+
+let projet: Projet | undefined = undefined;
+projets.forEach((e) => {
+  console.log("Test: '"+e.url+"' est-il '"+url+"' ?");
+  if (e.url == url){
+    console.log("Oui !");
+    projet = e;
+  }
+  console.log("Non !");
+});
+if (projet == undefined){
+  console.log("Non trouvé ! Abandon !");
+  showError({
+    status: 404,
+    statusText: 'Mission introuvable',
+  })
+  throw new Error("404: Mission introuvable");
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
