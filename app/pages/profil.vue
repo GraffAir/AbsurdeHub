70efccb833f085e57876e0f46770ea3b189faede
Rafaël Graff <rafael.graff@etu.umontpellier.fr>
2026-02-05 10:33:31 +0100

    feat: biographie

--- app/pages/profil.vue
+++ app/pages/profil.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
-<WidgetVedette />
+  <main>
+    <section>
+      <h1>Rafaël Graff</h1>
+      <p>
+        Créateur, développeur et concepteur avec une appétence pour la gestion de projet.
+
+        J'ai toujours été de nature à initier et porter des projets moi-même, à innover, à créer des expériences.
+        Ça va donc sans dire que, quand j'ai été introduit à l'informatique enfant,
+        la programmation et aux possibilités infinies que cela permettait, j'ai tout de suite été conquis.
+      </p>
+      <p>
+        Des années plus tard, cette passion ne s'est pas arrêtée, au contraire.
+        J'ai intégré l'IUT Montpellier-Sète en BUT Informatique spécialité management du système d'information,
+        cherchant à mettre la <strong>gestion de projet</strong> et l'informatique au cœur de mon projet professionnel.
+        Post-BUT, je vise une poursuite d'études dans une formation liée au management.
+      </p>
+      <p>
+        Aujourd'hui, je cherche à continuer cette passion pour l'informatique dans le cadre d'un stage ou d'une alternance,
+        particulièrement dans des domaines liées à la gestion de projet ou au management du système d'information.
+      </p>
+    </section>
+    <section>
+      <WidgetVedette />
+    </section>
+  </main>
+
 </template>
 
 <style>
+main {
+  display: flex;
+  margin: 40px;
+}
 
+main section {
+  width:50%;
+}
 </style>
