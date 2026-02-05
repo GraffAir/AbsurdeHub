6bec9b0058b22b394b2cdf557a2ac86807edf2ca
GraffAir <humainabsurde@proton.me>
2026-02-05 01:37:50 +0100

    chore: importer extension markdown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/strapi'],
+  modules: ['@nuxtjs/strapi', '@nuxtjs/mdc'],
   strapi: {
     url: 'http://51.83.76.32:1337',
       prefix: '/api',
