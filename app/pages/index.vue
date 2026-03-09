b1721ff3500f9244abb34acbcacace6227288f65
GraffAir <humainabsurde@proton.me>
2026-03-09 22:17:43 +0100

    feat: composant CRT

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,41 +1,40 @@
 <template>
   <div>
-    <div class="titre">
-      <p class="titre-nom">Rafaël Graff</p>
-      <h1>Créer du sens<br/> dans l&apos;<span class="titre-accent">absurde</span></h1>
-      <div class="titre-boutons">
-        <BoutonTitre to="/profil" texte="Commencer l'expédition"/>
-        <BoutonTitre to="/profil" texte="Mon profil" />
-        <BoutonTitre to="/profil" texte="Mes missions" />
+    <crt>
+      <div class="titre">
+        <p class="titre-nom">Rafaël Graff</p>
+        <h1>Créer du sens<br/> dans l&apos;<span class="titre-accent">absurde</span></h1>
+        <div class="titre-boutons">
+          <BoutonTitre to="/profil" texte="Commencer l'expédition"/>
+          <BoutonTitre to="/profil" texte="Mon profil" />
+          <BoutonTitre to="/profil" texte="Mes missions" />
+        </div>
       </div>
+    </crt>
+    <div>
+      <h1>Autre test</h1>
     </div>
-
-
   </div>
 </template>
+
 <script setup lang="ts">
 </script>
+
 <style scoped>
 h1 {
-  font-size:52pt;
+  font-size: 52pt;
   font-family: 'Boldonse', sans-serif;
 }
-
 .titre {
-  padding-top:10%;
-  padding-left:10%;
-  color:white;
-  z-index:2;
+  padding-top: 10%;
+  padding-left: 10%;
+  color: white;
 }
-
 .titre-accent {
   font-weight: 900;
   color: var(--accent-color);
 }
-
 .titre-boutons {
-  margin-top:5vh;
+  margin-top: 5vh;
 }
-
-
 </style>
